6e316f8ba2eb4e52a3e02068509df6f81270e47b
tomscotch <tomscotch@web.de>
2016-09-13 05:51:00 +0000

    auto

--- qciengine/start_qciengine.sh
+++ qciengine/start_qciengine.sh
@@ -1,1 +1,1 @@
-docker run --name qciengine -p 5002:5002 -d --net=host --restart=on-failure:9 -v /home/pi/workspace/qciengine:/data/ scotch/qiciengine
+docker run --name qciengine -p 5002:5002 -d --net=host --restart=on-failure:9 -v /home/pi/workspace/qciengine:/data/ scotch/qciengine
---
+++ web-terminal/start_web-terminal.sh
@@ -0,0 +1,1 @@
+docker run -d --net=host -p 3000:3000 --name web-terminal scotch/web-terminal
