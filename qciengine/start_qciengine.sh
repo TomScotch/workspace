afe57f5b93b2be7ed611cd4bfb69344a614dc1b9
tomscotch <tomscotch@web.de>
2016-09-13 02:57:44 +0000

    v.1.6

--- qciengine/start_qciengine.sh
+++ qciengine/start_qciengine.sh
@@ -1,1 +1,1 @@
-docker run --name qciengine --restart=always -p 5002:5002 -d --net=host --restart=on-failure:9 -v /home/pi/workspace/qciengine:/data/ scotch/qciengine
+docker run --name qciengine -p 5002:5002 -d --net=host --restart=on-failure:9 -v /home/pi/workspace/qciengine:/data/ scotch/qiciengine
