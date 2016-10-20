1763952d062e70c97fd204fabda1988cdd92a52d
tomscotch <tomscotch@web.de>
2016-10-20 06:15:00 +0200

    wiki

--- magenta/create.sh
+++
@@ -1,4 +0,0 @@
-#create container
-#create.sh
-docker create --net=host -p 8888:8888 -t -i --name ${PWD##*/} scotch/${PWD##*/}
-
---
+++ scp/remove_tags.sh
@@ -0,0 +1,3 @@
+sed -e s/quot//g -i /media/scps/* && sed -e s/nbps//g -i /media/scps/* && sed -e s/8211 x//g -i /media/scps/*
+
+
