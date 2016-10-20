a8f2468756c29dcf9721eaa06347faad8c90f62d
tomscotch <tomscotch@web.de>
2016-10-20 07:49:54 +0200

    cobe scp generator

---
+++ cobe/train_scps.sh
@@ -0,0 +1,6 @@
+for f in /media/scps/scp-****.html.txt
+do
+  name=${f#/media/scps/}
+  docker exec cobe cobe learn "/opt/scps/$name"
+  docker cp cobe:/opt/cobe.brain /home/pi/workspace/cobe/
+done
--- darknet/train_scps.sh
+++ darknet/train_scps.sh
@@ -1,7 +1,7 @@
-for f in /media/scps/scp-****.html.txt do
+name=""
+for f in /media/scps/scp-****.html.txt
+do
   name=${f#/media/scps/}
-  nam=${f%.html.txt}
-  echo "$nam"
   docker exec darknet ./darknet rnn train cfg/rnn.cfg -file "/opt/scps/$name"
   docker cp darknet:/opt/rnn.backup /home/pi/workspace/darknet/
 done
