979095b5af5ec292f381adec283b40820c3c782a
tomscotch <tomscotch@web.de>
2016-10-26 08:52:07 +0200

    youtube problems fixed

--- clip-creator/run-all.sh
+++ clip-creator/run-all.sh
@@ -1,13 +1,9 @@
 for f in /opt/scps/*.wav
   do
-    if [ -f "$f.ogv" ]
+    if [ -f "$f.mp4" ]
       then
         echo "skipped : " $f " : already existing"
     else
-        files=(/opt/electricsheep/*.mp4)
-        N=${#files[@]}
-        ((N=RANDOM%N))
-        randf=${files[$N]}
-    ffmpeg -i $randf -i $f $f.ogv
+      ffmpeg -i /opt/scp.flv -i $f -shortest $f.mp4
   fi
 done
