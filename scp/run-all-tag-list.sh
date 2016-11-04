c1d9492adc760fdca3e7ad319b5da81a0851a288
tomscotch <tomscotch@web.de>
2016-11-04 09:05:54 +0100

    7

--- scp/run-all-tag-list.sh
+++ scp/run-all-tag-list.sh
@@ -1,1 +1,7 @@
-./tag_clean.sh $(cat tag_list.txt) ''
+sed -e s/'?I will'/'I will'/g -i /media/scps/*.txt
+sed -e s/'2000animal'/'animal'/g -i /media/scps/*.txt
+x=$(< tag_list.txt)
+for tag in $x
+do
+  ./tag_clean.sh $tag
+done
--- scp/tag_clean.sh
+++ scp/tag_clean.sh
@@ -1,7 +1,4 @@
-sed -e s/'?I will'/'I will'/g -i /media/scps/*.txt
-sed -e s/'2000animal'/'animal'/g -i /media/scps/*.txt
-
 for f in /opt/scps/*.txt
   do
-    sed -e s/'$1'/'$2'/g -i /media/scps/*.txt
+    sed -e s/$1//g -i /media/scps/*.txt
   done
