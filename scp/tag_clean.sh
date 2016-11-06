77aa4d06489359d644c7da3daa7f7f96226388f1
tomscotch <tomscotch@web.de>
2016-11-06 03:19:45 +0100

    tag_clean buggy

--- scp/tag_clean.sh
+++ scp/tag_clean.sh
@@ -1,6 +1,6 @@
-for f in $(find /media/scps/*.txt -type f -print | xargs grep "$(cat tag_list.txt)")
+for f in $(find /media/scps/*.txt -type f -print | xargs grep "$(cat list_tag.txt)")
   do
     f=$(echo $f | tr ";" "\n")
-    file=f[0] ; exp=f[1]
+    file= $f[0] ; exp= $f[1]
     sed -e s/$exp//g -i $file
   done
