2db3ee0354c291d8b4cd8d7f798bbcf36e6aced1
tomscotch <tomscotch@web.de>
2016-11-06 03:57:39 +0100

    nigthly

--- scp/clean.sh
+++
@@ -1,2 +0,0 @@
-rm /media/scps/*.txt ; rm /media/scps/*.log ;
-./setup.sh && ./process_scps.sh && ./dump_scps.sh && ./removetags.sh && ./process_text.sh && ./test_scps.sh
--- scp/tag_clean.sh
+++ scp/tag_clean.sh
@@ -1,6 +1,9 @@
-for f in $(find /media/scps/*.txt -type f -print | xargs grep "$(cat list_tag.txt)")
+for e in tag_list.txt
   do
-    f=$(echo $f | tr ";" "\n")
-    file= $f[0] ; exp= $f[1]
-    sed -e s/$exp//g -i $file
+    for f in /media/scps/*.txt
+      do
+        sed -e s/$e//g -i $f
+      done
   done
+
+echo "done removing tags"
