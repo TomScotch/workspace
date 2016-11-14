d1da5d18012a8e0d3cb756e4cc37bc72e0a04bd3
tomscotch <tomscotch@web.de>
2016-11-14 01:18:40 +0100

    nightly

--- clip-creator/run-all.sh
+++ clip-creator/run-all.sh
@@ -7,7 +7,7 @@ for f in /opt/scps/*.wav
      ffmpeg -i $f \
      -stream_loop -1 \
      -i /opt/sheep/scp.avi \
-     -c:v libx264 -preset medium \
+     -c:v libx264 -preset superfast \
      -shortest -fflags +genpts $f.mp4
     fi
 done
--- youtube-uploader/remove.py
+++ youtube-uploader/remove.py
@@ -6,6 +6,6 @@ for arg in sys.argv:
   if c<255 :
     txt = txt+"".join(i for i in arg if ord(i)<128)
     txt = txt + " "
-    c=c+1
+    c=c+len(txt)
 
 print txt.decode(encoding='UTF-8',errors='strict')
