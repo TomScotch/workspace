14e462eb39660445858ae857fe83f04a9eb4fd38
tomscotch <tomscotch@web.de>
2016-12-23 18:24:27 +0100

    marytts

--- marytts/create.sh
+++
@@ -1,4 +0,0 @@
-#create container
-#create.sh
-docker create -p 59125:59125 --net=host -t -i --name ${PWD##*/} scotch/${PWD##*/} bash
-
--- marytts/exec.sh
+++
@@ -1,3 +0,0 @@
-#execute commands
-#exec.sh
-docker exec ${PWD##*/} sudo -u mary ./text2wav.sh $1 $2
