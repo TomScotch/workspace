695ce033bae01a7c49277e690d90fc7dd2905934
tomscotch <tomscotch@web.de>
2016-12-23 19:12:48 +0100

    text2imag2 storyteller

---
+++ text2image/pull.sh
@@ -0,0 +1,1 @@
+docker pull somatic/text2image
--- word-rnn/install-deps.sh
+++
@@ -1,2 +0,0 @@
-docker exec ${PWD##*/} $1 'docker luarocks install nngraph && luarocks install nninit && luarocks install optim && luarocks install nn && luarocks install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks/'
-# lrexlib-pcre PCRE_LIBDIR=/lib/x86_64-linux-gnu
