828b3f40a4a8d0e5e5d04b15ba93566c010abf69
tomscotch <tomscotch@web.de>
2016-12-23 18:06:35 +0100

    daily

--- marytts/run.sh
+++
@@ -1,1 +0,0 @@
-curl "http://localhost:59125/process?INPUT_TYPE=TEXT&INPUT_TEXT=$1&AUDIO=WAVE&LOCALE=en_US&OUTPUT_TYPE=AUDIO" > output.wav
---
+++ word-rnn/install-deps.sh
@@ -0,0 +1,1 @@
+docker exec ${PWD##*/} $1 'docker luarocks install nngraph && luarocks install nninit && luarocks install optim && luarocks install nn && luarocks install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks/ && luarocks install lrexlib-pcre PCRE_LIBDIR=/lib/x86_64-linux-gnu'
