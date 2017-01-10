a5f83ef494c868e96085c8f8461b7af6442bd823
tomscotch <tomscotch@web.de>
2017-01-10 23:17:20 +0100

    a

---
+++ scripts/fill_scps.sh
@@ -0,0 +1,9 @@
+for f in $(cat /media/scps/.uploaded) ; do echo "" > /media/scps/$f.html.dump.txt.wav.mp4 ; done
+
+for f in $(cat /media/scps/.uploaded) ; do echo "" > /media/scps/$f.html.dump.txt.wav ; done
+
+for f in $(cat /media/scps/.uploaded) ; do echo "" > /media/scps/$f.html.dump.txt ; done
+
+for f in $(cat /media/scps/.uploaded) ; do echo "" > /media/scps/$f.html.dump ; done
+
+for f in $(cat /media/scps/.uploaded) ; do echo "" > /media/scps/$f.html ; done
--- scripts/fill_video_with_null.sh
+++
@@ -1,1 +0,0 @@
-for f in $(cat /media/scps/.uploaded) ; do echo "" > /media/scps/$f.html.dump.txt.wav.mp4 ; done
