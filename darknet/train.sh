7a16d9ab0bc1a04e66c2f32ec7b88165483d10e2
tomscotch <tomscotch@web.de>
2017-01-10 23:08:36 +0100

    nightly 10.01.2017

--- darknet/train.sh
+++ darknet/train.sh
@@ -1,1 +1,1 @@
-docker exec darknet ./darknet rnn train cfg/rnn.train.cfg /opt/rnn.backup  -file data.txt
+nvidia-docker exec darknet ./darknet rnn train cfg/rnn.train.cfg /opt/rnn.backup  -file data.txt
