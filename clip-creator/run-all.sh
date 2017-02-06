0f6f5ca611c432a1d573d323ec745b20dd20ce86
root <tomscotch@web.de>
2017-02-06 15:37:32 +0100

    06.02.17

--- clip-creator/run-all.sh
+++ clip-creator/run-all.sh
@@ -8,31 +8,33 @@ for f in /opt/scps/*.mp3
         echo "skipped : " $f " : already existing"
     else
       if [ "$test" == "" ];then
-         x=$(ls -1 /opt/scps/$name/)
-       if [ -f $x.crush ] ; then
+         x=$(ls -1 /opt/scps/$name/ | head -1 | tail -1)
+       if [ -f "$x.crush" ] ; then
          ffmpeg \
           -i $f \
 	  -f image2 \
 	  -loop 1 \
-	  -i $x.crush \
+	  -i /opt/scps/$name/$x.crush \
 	  -r 15 \
 	  -c:v libx264 \
 	  -crf 18 \
 	  -tune stillimage \
 	  -preset ultrafast \
-	  -shortest $f.mp4
+	  -shortest \
+	   $f.mp4
       else
         ffmpeg \
           -i $f \
           -f image2 \
           -loop 1 \
-          -i $x \
+          -i /opt/scps/$name/$x \
           -r 15 \
           -c:v libx264 \
           -crf 18 \
           -tune stillimage \
           -preset ultrafast \
-          -shortest $f.mp4 
+          -shortest \
+	  $f.mp4
      fi
    else
       echo $name 'already uploaded'
