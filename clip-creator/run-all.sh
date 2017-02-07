e2b79e154a9b2dc75201cdcd5f92a1ed13772b1d
root <tomscotch@web.de>
2017-02-07 23:47:14 +0100

    07.02.17

--- clip-creator/run-all.sh
+++ clip-creator/run-all.sh
@@ -9,20 +9,6 @@ for f in /opt/scps/*.mp3
     else
       if [ "$test" == "" ];then
          x=$(ls -1 /opt/scps/$name/ | head -1 | tail -1)
-       if [ -f "$x.crush" ] ; then
-         ffmpeg \
-          -i $f \
-	  -f image2 \
-	  -loop 1 \
-	  -i /opt/scps/$name/$x.crush \
-	  -r 15 \
-	  -c:v libx264 \
-	  -crf 18 \
-	  -tune stillimage \
-	  -preset ultrafast \
-	  -shortest \
-	   $f.mp4.tmp
-      else
         ffmpeg \
           -i $f \
           -f image2 \
@@ -35,11 +21,10 @@ for f in /opt/scps/*.mp3
           -preset ultrafast \
           -shortest \
 	  $f.mp4.temp
-     fi
-   else
-      echo $name 'already uploaded'
+      else
+        echo $name 'already uploaded'
+      fi
    fi
-  fi
 mv $f.mp4.temp $f.mp4
 done
 echo "continue with youtube uploader"
