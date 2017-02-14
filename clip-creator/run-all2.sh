29ae7826a1ab59f75ab155745d844807b8effa82
root <tomscotch@web.de>
2017-02-14 22:46:02 +0100

    14.02.17

---
+++ clip-creator/run-all2.sh
@@ -0,0 +1,22 @@
+for f in /media/scps/*.mp3
+  do
+  name=${f#/media/scps/}
+  name=${name%.html.*}
+  test=$(grep $name /media/scps/.uploaded)
+    if [ -f "$f.mp4" ] ; then
+        echo "skipped : " $f " : already existing"
+    else
+         x=$(ls -1 /media/scps/$name/ | head -1 | tail -1)
+          ./ffmpeg \
+	  -hwaccel cuvid \
+          -i $f \
+          -f image2 \
+          -loop 1 \
+          -i /media/scps/$name/$x \
+          -c:v h264_cuvid \
+          -shortest \
+	  -vcodec h264_nvenc \
+	  $f.mp4
+   fi
+done
+echo "continue with youtube uploader"
