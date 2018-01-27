28b46a1149ff2197e91bfb11426fbee19a513bc3
TomScotch <tomscotch@localhost.localdomain>
2018-01-27 15:04:15 +0100

    Sat Jan 27 15:04:15 CET 2018

--- mimic/lang/es_lang/es_lang.h
+++
@@ -1,5 +0,0 @@
-#include "mimic.h"
-
-void es_init(cst_voice *v);
-void es_init_tokenizer(cst_voice *v);
-cst_val *es_tokentowords(cst_item *token);
--- mimic/lang/vid_gb_ap/voxdefs.h
+++
@@ -1,6 +0,0 @@
-#define VOXNAME vid_gb_ap
-#define REGISTER_VOX register_vid_gb_ap
-#define UNREGISTER_VOX unregister_vid_gb_ap
-#define VOXHUMAN "ap"
-#define VOXGENDER "unknown"
-#define VOXVERSION 1.0
