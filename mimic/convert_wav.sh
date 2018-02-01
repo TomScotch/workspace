aa1a83f1a2ab39c944deb6b1728b50e51ad27400
tomscotch <tomscotch@github.com>
2018-02-01 06:49:25 +0100

    Do 1. Feb 06:49:25 CET 2018

--- mimic/convert_wav.sh
+++ mimic/convert_wav.sh
@@ -1,4 +1,8 @@
 for x in $(ls /media/scps/*.wav -1) ; do
+if [ -f $f".mp3" ]; then
+echo $f "skipped"
+else
 ffmpeg -i $x $x.mp3 ;
-echo $x.mp3
-; done
+echo $x.mp3 "erstellt"
+fi
+done
--- url2img/convert_Image.sh
+++ url2img/convert_Image.sh
@@ -1,3 +1,7 @@
 for x in $(ls /media/scps/*.png -1) ; do
-convert $x $x.jpg
-; done
+if [ -f $f".jpg" ]; then
+echo $f "skipped" ;
+else
+convert $x $x.jpg ;
+fi
+done
