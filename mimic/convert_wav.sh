e99f4f709f848dce85a5e56d0ec2bacefb5fa369
tomscotch <tomscotch@github.com>
2018-02-02 01:36:02 +0100

    Fr 2. Feb 01:36:02 CET 2018

--- mimic/convert_wav.sh
+++ mimic/convert_wav.sh
@@ -1,8 +1,8 @@
-for x in $(ls /media/scps/*.wav -1) ; do
-if [ -f $f".mp3" ]; then
-echo $f "skipped"
+for f in $(ls /media/scps/*.wav -1) ; do
+if [ -f $f".mp3" ] ; then
+echo $f".mp3 skipped"
 else
-ffmpeg -i $x $x.mp3 ;
-echo $x.mp3 "erstellt"
+ffmpeg -i $f $f.mp3
+echo $f.mp3 "erstellt"
 fi
 done
