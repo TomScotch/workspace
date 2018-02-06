7d2ed7dcc977ba53b3985be5d6038e58f070e325
TomScotch <tomscotch@localhost.localdomain>
2018-02-06 19:16:16 +0100

    tomscotch:1517940976

--- waveCleaner/run.sh
+++ waveCleaner/run.sh
@@ -1,1 +1,1 @@
-sox $1 -n trim 0 1.5 noiseprof speech.noise-profil ; sox $1 $2 norm noisered speech.noise-profile 0.5
+sox $1 -n trim 0 0.5 noiseprof speech.noise-profil ; sox $1 $2 norm noisered speech.noise-profil 0.25
