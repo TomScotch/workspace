0670226242c69a7a60598681c57e29537e45bdd9
TomScotch <tomscotch@localhost.localdomain>
2018-02-06 15:36:06 +0100

    Tue Feb  6 15:36:06 CET 2018

--- scp/show-all.sh
+++ scp/show-all.sh
@@ -1,9 +1,9 @@
 echo "website : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.html') && \
 echo "extract : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.dump') && \
 echo "text : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.txt') && \
-echo "wav : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.wav') && \
+#echo "wav : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.wav') && \
 echo "mp3 : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.mp3') && \
-echo "mp4 : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.mp4') && \
+#echo "mp4 : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.mp4') && \
 echo "png : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.png') && \
 echo "jpg : " $(bash /home/tomscotch/workspace/scripts/count_files.sh '/media/scps/*.jpg') && \
 ./find-zero.sh
---
+++ waveCleaner/run.sh
@@ -0,0 +1,1 @@
+sox $1 -n trim 0 1.5 noiseprof speech.noise-profil ; sox $1 $2 norm noisered speech.noise-profile 0.5
