c0a5f906348659f1005ff879f2cdccbeae699a1e
Thomas <TomScotch@web.de>
2018-09-04 03:02:13 +0200

    Update rec.sh

--- guitarRecord/rec.sh
+++ guitarRecord/rec.sh
@@ -1,8 +1,11 @@
-clear ;
 timestamp=$(date +%s);
-sox -c 1 -G norm -20 -d $timestamp.wav ; # compand 0.3,1 -90,-90,-70,-70,-60,-20,0,0 -5 0 0.2 ;
-#sox $timestamp.wav -n noiseprof noise-profile;
-sox $timestamp.wav $timestamp-clean.wav noisered noise-profile 0.01 ;
-sox $timestamp-clean.wav $timestamp.wav norm -20 ;
-rm $timestamp-clean.wav ; clear ;
-play $timestamp.wav ; clear
+rec -G --replay-gain track $timestamp.wav
+
+##
+timestamp=$(date +%s) ;
+rec -G --replay-gain track $timestamp.wav & 
+play -G --replay-gain track --norm $1 &&
+killall rec ; 
+sleep 7 ;
+./convex.sh $timestamp.wav ; 
+exit
